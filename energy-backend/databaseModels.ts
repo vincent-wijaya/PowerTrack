86ea2794112cb748236bb45bb1fcffa17c5a1a80
Aspen Belle <aspen@jelly-snakes.com>
2024-05-06 16:59:51 +1000

    feature/Consume and store events from kafka into the database FIT-21

--- energy-backend/databaseModels.ts
+++ energy-backend/databaseModels.ts
@@ -3,16 +3,19 @@ import { Sequelize, DataTypes } from 'sequelize';
 const sequelize = new Sequelize('postgres://postgres:password@db:5432/retailerEnergy',
   {
     dialect: 'postgres',
+    define: {
+      timestamps: false,
+    },
     protocol: 'postgres',
     dialectOptions: {}
   })
 
-/* Setup models to apply and represent the database schema. All properties are non-nullable,
- * and the only nullable foreign keys exist in the entity_goal and report tables.
- * Specific comments are added only to the first table, as all other follow the same format */
+// Setup models to apply and represent the database schema. All properties are non-nullable,
+// and the only nullable foreign keys exist in the entity_goal and report tables.
+// Specific comments are added only to the first table, as all other follow the same format
 
-/* Create suburb related tables
- * Properties defined from the schema document */
+// Create suburb related tables
+// Properties defined from the schema document
 const Suburb = sequelize.define("suburb", //set the name of the table
   {
     id: { // Name of the property
@@ -51,11 +54,16 @@ const SuburbConsumption = sequelize.define("suburb_consumption",
       primaryKey: true,
       type: DataTypes.DATE,
       allowNull: false
+    },
+    suburb_id: {
+      primaryKey: true,
+      type: DataTypes.INTEGER,
+      allowNull: false
     }
   }, { freezeTableName: true })
 
-/* Link suburb to suburb consumption
- * One to many relation */
+// Link suburb to suburb consumption
+// One to many relation
 Suburb.hasMany(SuburbConsumption, {
   foreignKey: { name: "suburb_id", allowNull: false }
 })
@@ -64,19 +72,14 @@ SuburbConsumption.belongsTo(Suburb, {
 })
 
 
-/* Create consumer related tables
- * Properties defined from the schema document */
+// Create consumer related tables
+// Properties defined from the schema document
 const SellingPrice = sequelize.define("selling_price",
   {
     date: {
       primaryKey: true,
       type: DataTypes.DATE,
       allowNull: false
-    },
-    id: {
-      type: DataTypes.BIGINT,
-      allowNull: false,
-      unique: true
     },
     amount: {
       type: DataTypes.DECIMAL,
@@ -109,11 +112,16 @@ const ConsumerConsumption = sequelize.define("consumer_consumption",
       type: DataTypes.DATE,
       allowNull: false,
       primaryKey: true
+    },
+    consumer_id: {
+      primaryKey: true,
+      type: DataTypes.INTEGER,
+      allowNull: false
     }
   }, { freezeTableName: true })
 
-/* Link consumer to consumer consumption
- * One to many relation */
+// Link consumer to consumer consumption
+// One to many relation
 Consumer.hasMany(ConsumerConsumption, {
   foreignKey: { name: "consumer_id", allowNull: false }
 })
@@ -121,13 +129,9 @@ ConsumerConsumption.belongsTo(Consumer, {
   foreignKey: { name: "consumer_id", allowNull: false }
 })
 
-/* Link selling price to consumer
-  * One to many relation */
-SellingPrice.hasMany(Consumer, { foreignKey: { name: "selling_price_id", allowNull: false } })
-Consumer.belongsTo(SellingPrice, { foreignKey: { name: "selling_price_id", allowNull: false } })
 
-/* Link consumer to suburb
- * Many to one relation */
+// Link consumer to suburb
+// Many to one relation
 Suburb.hasMany(Consumer, {
   foreignKey: { name: "suburb_id", allowNull: false }
 })
@@ -135,19 +139,14 @@ Consumer.belongsTo(Suburb, {
   foreignKey: { name: "suburb_id", allowNull: false }
 })
 
-/* Create generator related tables
- * Properties defined from the schema document */
+// Create generator related tables
+// Properties defined from the schema document
 const SpotPrice = sequelize.define("spot_price",
   {
     date: {
       primaryKey: true,
       type: DataTypes.DATE,
       allowNull: false
-    },
-    id: {
-      type: DataTypes.BIGINT,
-      allowNull: false,
-      unique: true
     },
     amount: {
       type: DataTypes.DECIMAL,
@@ -193,28 +192,29 @@ const EnergyGeneration = sequelize.define("energy_generation",
       type: DataTypes.DATE,
       allowNull: false,
       primaryKey: true
+    },
+    energy_generator_id: {
+      primaryKey: true,
+      type: DataTypes.INTEGER,
+      allowNull: false
     }
   }, { freezeTableName: true })
 
-/* Link GeneratorType to EnergyGenerator
- * One to many relation */
+// Link GeneratorType to EnergyGenerator
+// One to many relation
 GeneratorType.hasMany(EnergyGenerator, { foreignKey: { name: "generator_type_id", allowNull: false } })
 EnergyGenerator.belongsTo(GeneratorType, { foreignKey: { name: "generator_type_id", allowNull: false } })
-/* Link EnergyGenerator to EnergyGeneration
- * One to many relation */
+// Link EnergyGenerator to EnergyGeneration
+// One to many relation
 EnergyGenerator.hasMany(EnergyGeneration, { foreignKey: { name: "energy_generator_id", allowNull: false } })
 EnergyGeneration.belongsTo(EnergyGenerator, { foreignKey: { name: "energy_generator_id", allowNull: false } })
-/* Link SpotPrice to EnergyGeneration
-  * One to many relation */
-SpotPrice.hasMany(EnergyGeneration, { foreignKey: { name: "spot_price_id", allowNull: false } })
-EnergyGeneration.belongsTo(SpotPrice, { foreignKey: { name: "spot_price_id", allowNull: false } })
-/* Link suburb to energy generator
- * One to many relation */
+// Link suburb to energy generator
+// One to many relation
 Suburb.hasMany(EnergyGenerator, { foreignKey: { name: "suburb_id", allowNull: false } })
 EnergyGenerator.belongsTo(Suburb, { foreignKey: { name: "suburb_id", allowNull: false } })
 
-/* Create goal and report related tables
- * Properties defined from the schema document */
+// Create goal and report related tables
+// Properties defined from the schema document
 const GoalType = sequelize.define("goal_type",
   {
     id: {
@@ -277,29 +277,89 @@ const WarningType = sequelize.define("warning_type",
     }
   }
 )
-/* Link goal type to entity goal
- * One to many relation */
+// Link goal type to entity goal
+// One to many relation
 GoalType.hasMany(Goal, { foreignKey: { name: "goal_id", allowNull: false } })
 Goal.belongsTo(GoalType, { foreignKey: { name: "goal_id", allowNull: false } })
-/* Link consumer to entity goal
- * One to many relation */
+// Link consumer to entity goal
+// One to many relation
 Consumer.hasMany(Goal, { foreignKey: { name: "consumer_id", allowNull: true } })
 Goal.belongsTo(Consumer, { foreignKey: { name: "consumer_id", allowNull: true } })
-/* Link suburb to report
- * One to many relation
- * Nulls are allowed */
+// Link suburb to report
+// One to many relation
+// Nulls are allowed
 Suburb.hasMany(Reports, { foreignKey: { name: "suburb_id", allowNull: true } })
 Reports.belongsTo(Suburb, { foreignKey: { name: "suburb_id", allowNull: true } })
-/* Link consumer to report
- * One to many relation
- * Nulls are allowed */
+// Link consumer to report
+// One to many relation
+// Nulls are allowed
 Consumer.hasMany(Reports, { foreignKey: { name: "consumer_id", allowNull: true } })
 Reports.belongsTo(Consumer, { foreignKey: { name: "consumer_id", allowNull: true } })
 
-/* Link goal and warning type
- * One to One relation */
+// Link goal and warning type
+// One to One relation */
 GoalType.hasOne(WarningType, { foreignKey: { name: "goal_type_id", allowNull: false } })
 WarningType.belongsTo(GoalType, { foreignKey: { name: "goal_type_id", allowNull: false } })
+
+/**
+ * Create and insert a number of fake suburb entries
+ * 
+ * @param count How many dummy entries to produce
+ * @returns A list of created suburb models
+ */
+async function makeDummySuburbs(count: number) {
+  let suburbs: any[] = []
+  for (let i = 0; i < count; i++) {
+    suburbs.push(await Suburb.create({
+      id: i,
+      name: i.toString(),
+      postcode: i * 1000,
+      state: 'vic',
+      latitude: 0.1 + i,
+      longitude: 0.1 + i
+    }))
+  }
+  return suburbs
+}
+
+/**
+* Create and insert a number of fake consumer entries
+* 
+* @param count How many dummy entries to produce
+* @returns A list of created consumer models
+*/
+async function makeDummyConsumers(count: number) {
+  let consumers: any[] = []
+  for (let i = 0; i < count; i++) {
+    consumers.push(await Consumer.create({
+      id: i,
+      high_priority: false,
+      street_address: "123, 123 street",
+      suburb_id: 1
+    }))
+  }
+  return consumers
+}
+
+/**
+* Create and insert a number of fake generator entries
+* 
+* @param count How many dummy entries to produce
+* @returns A list of created generator models
+*/
+async function makeDummyGenerators(count: number) {
+  let generators: any[] = []
+  await GeneratorType.create({ id: 1, category: "Gas", renewable: false })
+  for (let i = 0; i < count; i++) {
+    generators.push(await EnergyGenerator.create({
+      id: i,
+      name: "Loy Yang",
+      suburb_id: 1,
+      generator_type_id: 1
+    }))
+  }
+  return generators
+}
 
 async function setupDatabase() {
   // Write the database schema to the database
@@ -310,10 +370,24 @@ async function setupDatabase() {
     .catch((error) => {
       console.error("Unable to sync database schema: ", error);
     });
-  /* Convert relevant databases into timescale db hypertables */
+  // Convert relevant databases into timescale db hypertables
   await sequelize.query("SELECT create_hypertable('suburb_consumption', by_range('date'));");
   await sequelize.query("SELECT create_hypertable('consumer_consumption', by_range('date'));");
   await sequelize.query("SELECT create_hypertable('energy_generation', by_range('date'));");
+  await sequelize.query("SELECT create_hypertable('selling_price', by_range('date'));");
+  await sequelize.query("SELECT create_hypertable('spot_price', by_range('date'));");
+
+}
+
+/**
+ * Inserts 10 dummy suburbs, consumers and generators each wth id's from 0-10
+ * @param sequelize Connection to use to insert data
+ */
+async function makeDummyData(sequelize: Sequelize) {
+  // Insert dummy data
+  let dummysuburbs = await makeDummySuburbs(10)
+  let dummyConsumers = await makeDummyConsumers(10)
+  let dummyGenerators = await makeDummyGenerators(10)
 }
 
 
@@ -330,5 +404,8 @@ export {
   Goal,
   Reports,
   WarningType,
-  setupDatabase
+  SellingPrice,
+  SpotPrice,
+  setupDatabase,
+  makeDummyData
 };
