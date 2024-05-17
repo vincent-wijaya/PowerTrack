f3ac7c6177bc3545925011c941434a58bd658a9c
johnmck2099 <jmck0029@student.monash.edu>
2024-05-17 10:29:48 +1000

    save changes before adding maindashboard components

--- energy-frontend/src/app/mainDashboard/page.tsx
+++ energy-frontend/src/app/mainDashboard/page.tsx
@@ -3,15 +3,14 @@ import Headings from "@/app/template";
 import PageHeading from "@/components/pageHeading";
 export default function MainDashboard() {
   return (
-    <div>
       <>
         <PageHeading title="Home" />
 
         <div className="flex flex-col-2">
-          <div>{/* <InfoBox title="X Urgent Warnings" description="of all warnings" />*/}</div>
+          <div>
+          </div>
         </div>
       </>
-    </div>
   )
 }
 
