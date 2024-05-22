8924c2a3d45d856239fefc03031e4c78a56b8240
Aspen Belle <qrob0001@student.monash.edu>
2024-05-22 21:51:33 +1000

    Make the map use random energy amounts

--- energy-frontend/src/api/energyConsumption.tsx
+++ energy-frontend/src/api/energyConsumption.tsx
@@ -1,30 +1,32 @@
+import { randBetween } from "@/utils";
+
 export default async function fetchEnergyConsumption() {
     const dummyData = {
         energy: [
             {
               suburb_id: 20457,
               amount: 0,
-              date: "2024-04-17T09:06:41Z",
+              date: new Date().toISOString(),
             },
             {
               suburb_id: 20456,
-              amount: 10,
-              date: "2024-04-17T09:06:41Z",
+              amount: randBetween(10, 1000),
+              date: new Date().toISOString(),
             },
             {
               suburb_id: 20455,
-              amount: 50,
-              date: "2024-04-17T09:06:41Z",
+              amount: randBetween(10, 1000),
+              date:  new Date().toISOString(),
             },
             {
               suburb_id: 20454,
-              amount: 100,
-              date: "2024-04-17T09:06:41Z",
+              amount: randBetween(10, 1000),
+              date: new Date().toISOString(),
             },
             {
               suburb_id: 20453,
-              amount: 200,
-              date: "2024-04-17T09:06:41Z",
+              amount: randBetween(10, 1000),
+              date: new Date().toISOString(),
             },
            
           ],
