7a8a3a787e48f203e5744de2b546f54fec64d4db
Aspen Belle <qrob0001@student.monash.edu>
2024-05-22 21:51:15 +1000

    add in util for random number between

--- energy-frontend/src/utils.ts
+++ energy-frontend/src/utils.ts
@@ -1,1 +1,4 @@
 export const fetcher = (url: string) => fetch(url).then((r) => r.json());
+export const randBetween = (min:number, max:number):number => {
+    return Math.floor(Math.random() * (max - min) + min);
+}
