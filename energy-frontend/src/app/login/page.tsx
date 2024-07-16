5819a2ce59a4be86ca77a0d354fe23cbb0526865
johnmck2099 <jmck0029@student.monash.edu>
2024-07-16 15:44:58 +1000

    added login page

--- energy-frontend/src/app/login/page.tsx
+++ energy-frontend/src/app/login/page.tsx
@@ -1,8 +1,23 @@
+import Link from "next/link";
+
+
 export default function LoginPage() {
   return (
     <>
       <div className="bg-background">
-        hello
+        <div className="flex justify-center py-24">
+          <div className="text-white space-y-4">
+            <div>
+                <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">ORGANISATION ID</label>
+                <input className="bg-gray-50 border border-gray-300 text-black text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="john.doe@company.com" required />
+            </div>
+            <div className="bg-purple text-center rounded-lg p-2">
+              <Link href={'/main/mainDashboard'}>
+                Login
+              </Link>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   )
