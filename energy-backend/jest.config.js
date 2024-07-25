c94c93c73e8b05969ebdc19682b22c7db7fbb3cc
vincent-wijaya <vincentwijaya2001@yahoo.com>
2024-07-25 16:05:44 +1000

    Add configuration for coverage testing to Jest Configured jest to collect coverage report with threshold of 80%

--- energy-backend/jest.config.js
+++ energy-backend/jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  // Only include TypeScript test files
+  // Only include TypeScript test files from the tests folder
   testMatch: ['**/*.test.ts'],
   preset: 'ts-jest',
   testEnvironment: 'node',
@@ -10,4 +10,15 @@ module.exports = {
   },
   verbose: true,
   testTimeout: 10000,
+  collectCoverage: true,
+  coverageDirectory: "./coverage", // Ensure this directory is writeable and exists
+  coverageReporters: ["json", "lcov", "text", "clover"],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+  },
 };
