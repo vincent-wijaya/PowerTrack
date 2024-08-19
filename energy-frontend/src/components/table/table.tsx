5f73ab0914cfddcc66bc35558b09ece04e024894
johnmck2099 <jmck0029@student.monash.edu>
2024-08-19 10:42:40 +1000

    fixed styling for tables

--- energy-frontend/src/components/table/table.tsx
+++ energy-frontend/src/components/table/table.tsx
@@ -13,12 +13,12 @@ export default function Table({ columns, data, link }: TableProps) {
   const updatedColumns = link ? [...columns, "Action"] : columns;
 
   return (
-    <table className="flex flex-col flex-start flex-shrink-0 border-stroke border-2 rounded-sm">
-      <thead className="items-start flex-shrink-0 self-stretch">
-        <tr className="flex items-start flex-shrink-0 self-stretch">
+    <table className="table-auto border-stroke border-r-2">
+      <thead className="">
+        <tr className="">
         {updatedColumns.map((column, columnIndex) => (
           <th
-            className="flex p-4 items-start gap-8 flex-1 self-stretch border-stroke border-r-2 border-b-2"
+            className="p-4 items-start gap-8  border-stroke border-2"
             key={columnIndex}
           >
             <div className="font-inter text-white text-nowrap">{column}</div>
@@ -26,12 +26,12 @@ export default function Table({ columns, data, link }: TableProps) {
         ))}
         </tr>
       </thead>
-      <tbody className="items-start flex-shrink-0 self-stretch bg-itembg">
+      <tbody className="items-start bg-itembg">
         {data.map((row, rowIndex) => (
-          <tr key={rowIndex} className="flex items-start flex-shrink-0 self-stretch">
+          <tr key={rowIndex} className="">
             {columns.map((column, columnIndex) => (
               <td
-                className="px-3 py-8 items-start gap-8 flex-1 self-stretch border-stroke border-r-2 border-b-2"
+                className="px-3 py-8 items-start gap-8 border-stroke border-r-2 border-l-2 border-b-2"
                 key={columnIndex}
               >
                 <div className="font-inter text-white">
@@ -41,7 +41,7 @@ export default function Table({ columns, data, link }: TableProps) {
             ))}
             {link && (
               <td
-                className="px-3 py-8 items-start gap-8 flex-1 self-stretch border-stroke border-r-2 border-b-2"
+                className="px-3 py-8 items-start gap-8  border-stroke border-r-2 border-b-2"
                 key={`action-${rowIndex}`}
               >
                 <div className="font-inter text-center text-white bg-purple p-2 rounded-lg">
